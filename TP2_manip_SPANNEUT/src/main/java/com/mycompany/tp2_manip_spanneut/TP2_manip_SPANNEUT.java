29b42993277989ff2c294c60872ca05265b44b44
alexia <alexia@DESKTOP-3LIGDG0.education.lakanal.epf.fr>
2022-10-25 12:56:48 +0200

    début de l'exo 3

---
+++ TP2_manip_SPANNEUT/src/main/java/com/mycompany/tp2_manip_spanneut/TP2_manip_SPANNEUT.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tp2_manip_spanneut;
+
+/**
+ *
+ * @author alexia
+ */
+public class TP2_manip_SPANNEUT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Tartiflette assiette1 = new Tartiflette(500) ;
+        Tartiflette assiette2 = new Tartiflette(600) ;
+        Tartiflette assiette3 = assiette2 ;
+
+    }
+}
