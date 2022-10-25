f099672041d9241d5bf3c62acd812284c57d4775
alexia <alexia@DESKTOP-3LIGDG0.education.lakanal.epf.fr>
2022-10-25 12:43:22 +0200

    fin Exo 2 conversion de la temperature en fonction du choix de l'utilisateur

--- TP2_convertisseurObjet_SPANNEUT/src/main/java/com/mycompany/tp2_convertisseurobjet_spanneut/Convertisseur.java
+++ TP2_convertisseurObjet_SPANNEUT/src/main/java/com/mycompany/tp2_convertisseurobjet_spanneut/Convertisseur.java
@@ -19,37 +19,37 @@ public class Convertisseur {
     
     public double CelciusVersKelvin(double TKelvin){
         nbConversions += 1 ;
-        TKelvin = 50 + 273.15; 
+        TKelvin = TKelvin + 273.15; 
         return TKelvin;
     }
     
     public double KelvinVersCelcius(double TCelcius){
         nbConversions += 1 ;
-        TCelcius = 500 - 273.15;
+        TCelcius = TCelcius - 273.15;
         return TCelcius;
     }
     
     public double FarenheitVersCelcius(double TFC){
         nbConversions += 1 ;
-        TFC = (32 - 32)*5/9;
+        TFC = (TFC - 32)*5/9;
         return TFC;
     }
     
     public double CelciusVersFarenheit(double TCF){
        nbConversions += 1 ;
-       TCF = (0 * 9/5) + 32;
+       TCF = (TCF * 9/5) + 32;
        return TCF;
     }
     
     public double FarenheitVersKelvin(double TFK){
         nbConversions += 1 ;
-        TFK = (32 - 32)* 5/9 + 273.15;
+        TFK = (TFK - 32)* 5/9 + 273.15;
         return TFK;
     }
     
     public double KelvinVersFarenheit(double TKF){
         nbConversions += 1 ;
-        TKF = (32 - 273.15) * 9/5 + 32;
+        TKF = (TKF - 273.15) * 9/5 + 32;
         return TKF;
     }
     
--- TP2_convertisseurObjet_SPANNEUT/src/main/java/com/mycompany/tp2_convertisseurobjet_spanneut/TP2_convertisseurObjet_SPANNEUT.java
+++ TP2_convertisseurObjet_SPANNEUT/src/main/java/com/mycompany/tp2_convertisseurobjet_spanneut/TP2_convertisseurObjet_SPANNEUT.java
@@ -9,19 +9,64 @@ package com.mycompany.tp2_convertisseurobjet_spanneut;
  *
  * @author alexia
  */
+import java.util.Scanner;
 public class TP2_convertisseurObjet_SPANNEUT {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
         Convertisseur conversions = new Convertisseur();
-        conversions.CelciusVersFarenheit(0);
-        conversions.KelvinVersFarenheit(0);
-        conversions.FarenheitVersCelcius(0);
-        System.out.println(nbConversions);
+        //conversions.CelciusVersFarenheit(0);
+        //conversions.KelvinVersFarenheit(0);
+        //conversions.FarenheitVersCelcius(0);
+        System.out.println(conversions.nbConversions);
 
         Convertisseur conversions2 = new Convertisseur();
-        conversions2.KelvinVersFarenheit(0);
-        conversions2.KelvinVersFarenheit(0);
-        System.out.println(nbConversions);
+        conversions2.CelciusVersFarenheit(0);
+        conversions2.FarenheitVersCelcius(0);
+        System.out.println(conversions.nbConversions);
+       // scanner scanner = new scanner (System.in);
+        //system.out.println("")
+        //double nombre =scanner.nextDouble();
+    
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Affiche "Ecrire un nombre: "
+        System.out.println("chosisissez la conversion à faire");
+        System.out.println("Pour CelciusVersKelvin écrire: 1, Pour KelvinVersCelcius écrire: 2, Pour FarenheitVersCelcius écrire: 3, Pour CelciusVersFarenheit écrire: 4, Pour FarenheitVersKelvin écrire: 5, Pour KelvinVersFarenheit écrire: 6 ");
+        
+        //Lit l'entier écrit par l'utilisateur et l'affecte 
+        // à  la variable nombre du programme
+        double nombre = scanner.nextDouble();
+        // Affiche le nombre saisit par l'utilisateur
+        System.out.println("le nombre entré est :" + nombre);
+        
+        
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("écrire un nombre à convertir");
+        double nombre2 = scanner2.nextDouble();
+        System.out.println("le nombre entré est :" + nombre2);
+        
+        
+        if (nombre == 1){
+            conversions.CelciusVersKelvin(nombre2);
+            
+        }else if (nombre ==2){
+            conversions.KelvinVersCelcius(nombre2);            
+            
+        }else if (nombre ==3){
+            conversions.FarenheitVersCelcius(nombre2);             
+            
+        }else if (nombre ==4){
+            conversions.CelciusVersFarenheit(nombre2);            
+            
+        }else if (nombre ==5){
+            conversions.FarenheitVersKelvin(nombre2);            
+            
+        }else if (nombre ==6){
+            conversions.KelvinVersFarenheit(nombre2);            
+            
+        }
+   
     }
 }
